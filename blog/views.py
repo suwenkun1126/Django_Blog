67da4ee23a7ea1eaf4efc587fb1ec9bc628e267c
suwenkun1126 <suwenkun1126@126.com>
2017-12-22 22:53:24 +0800

    更新.gitignore

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request,'blog/index.html',context={
+        'title':'博客首页',
+        'welcome':'欢迎来到kuwe博客'
+    })
