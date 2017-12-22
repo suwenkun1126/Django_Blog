b3a944a22773c72cd7072329b0c4c61024d7876b
suwenkun1126 <suwenkun1126@126.com>
2017-12-22 21:13:26 +0800

    迁移数据库

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,14 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name=models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Tag(models.Model):
     name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title=models.CharField(max_length=70)
@@ -16,3 +22,6 @@ class Post(models.Model):
     category=models.ForeignKey(Category)
     tags=models.ManyToManyField(Tag,blank=True)
     author=models.ForeignKey(User)
+
+    def __str__(self):
+        return self.title
