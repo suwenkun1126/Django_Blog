988a4d9eb6b748bfff6c4afbeaa3312bef2f5304
suwenkun1126 <suwenkun1126@126.com>
2017-12-23 21:11:42 +0800

    增加文章详情页视图

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Category(models.Model):
     name=models.CharField(max_length=100)
@@ -25,3 +26,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail',kwargs={'pk':self.pk})
+
