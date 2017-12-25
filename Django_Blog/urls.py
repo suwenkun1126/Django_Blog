c5be6dd5d619d8b9b154b1f1b7d4c7e7f34b19c1
suwenkun1126 <suwenkun1126@126.com>
2017-12-25 14:07:39 +0800

    增加评论功能

--- Django_Blog/urls.py
+++ Django_Blog/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.conf.urls import url,include
 
 urlpatterns = [
-    url('admin/',admin.site.urls),
-    url('',include('blog.urls')),
+    url(r'admin/',admin.site.urls),
+    url(r'',include('blog.urls')),
+    url(r'',include('comments.urls')),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post,Category
 import markdown
+from comments.forms import CommentForm
+
 
 def index(request):
     post_list=Post.objects.all().order_by('-created_time')
@@ -14,7 +16,14 @@ def detail(request,pk):
                                     'markdown.extensions.codehilite',
                                     'markdown.extensions.toc',
                                 ])
-    return render(request,'blog/detail.html',context={'post':post})
+    form=CommentForm()
+    comment_list=post.comment_set.all()
+    context={
+        'post':post,
+        'form':form,
+        'comment_list':comment_list,
+    }
+    return render(request,'blog/detail.html',context=context)
 
 def archives(request,year,month):
     post_list=Post.objects.filter(created_time__year=year,
