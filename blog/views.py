773e0a5ded99b0653e159c26272edf56c5527ad5
suwenkun1126 <suwenkun1126@126.com>
2017-12-26 12:49:21 +0800

    实现标签云

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Post,Category
+from .models import Post,Category,Tag
 import markdown
 from comments.forms import CommentForm
 from django.views.generic import ListView,DetailView
@@ -87,4 +87,13 @@ class CategoryView(IndexView):
     def get_queryset(self):
         cate=get_object_or_404(Category,pk=self.kwargs.get('pk'))
         return super(CategoryView,self).get_queryset().filter(category=cate)
+
+class TagView(ListView):
+    model=Post
+    template_name='blog/index.html'
+    context_object_name='post_list'
+
+    def get_queryset(self):
+        tag=get_object_or_404(Tag,pk=self.kwargs.get('pk'))
+        return super(TagView,self).get_queryset().filter(tags=tag)
 
